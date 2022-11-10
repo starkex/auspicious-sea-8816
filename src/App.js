e318bfd1a26332fd4efb2d8ad4531672a3e68361
starkex <starkanonimity511@gmail.com>
2022-11-10 15:46:04 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
-
+// import './App.css';
+import React from 'react';
+import {Box, Image, Stack,Heading, Grid} from '@chakra-ui/react';
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <Stack direction={['row']} p={4} w={'80%'} position={'fixed'} top={0} bg={'whiteAlpha.300'}>
+        <Grid w={'100%'} templateColumns={['repeat(8,1fr)']} p={4}>
+        <Image src=''></Image>
+        <Heading as='h2' size='xs'>
+            Gifts
+        </Heading>
+        <Heading as='h2' size='xs'>
+            New
+        </Heading>
+        <Heading as='h3' size='xs'>
+            Womens
+        </Heading>
+        <Heading as='h4' size='xs'>
+            Mens
+        </Heading>
+        <Heading as='h5' size='xs'>
+            Kids
+        </Heading>
+        <Heading as='h6' size='xs'>
+            Cashmere
+        </Heading>
 
-    </div>
+        </Grid>
+    </Stack>
   );
 }
 
