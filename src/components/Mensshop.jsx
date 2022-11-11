33172449083566b38ed00456a57b133570fa8ec9
starkex <starkanonimity511@gmail.com>
2022-11-11 16:25:59 +0530

    product-mapping

--- src/components/Mensshop.jsx
+++ src/components/Mensshop.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import Products from './Products';
 
 
 function Mensshop() {
   return (
   <div>
     <Navbar/>
+    <Products/>
     <Footer/>
   </div>
   )
